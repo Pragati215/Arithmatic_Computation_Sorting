fac37e5425aa84bf436417e07938ba4068970510
Pragati215 <dhakanepragati4@gmail.com>
2020-10-25 22:22:15 +0530

    modified: Output stored in Dictionary

--- Arithmaticcomputation.sh
+++ Arithmaticcomputation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -i result
+
 read -p "enter a b c : " a b c
 
 s1=$(($a+($b*$c)));
@@ -18,3 +20,10 @@ s4=$((`expr $a%$b+$c`));
 
 echo " s4 - " $s4
 
+temp=0
+result[((temp++))]="$s1"
+result[((temp++))]="$s2"
+result[((temp++))]="$s3"
+result[((temp))]="$s4"
+
+echo "dictionary -" ${result[@]}
